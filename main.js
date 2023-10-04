90c2187ab2431d8f1e904b812531e1a942726dce
Victor Lieuthier <vgabriel@keener.io>
2023-10-04 09:58:14 -0300

    Ajustes no layout de segregaçao do codigo

---
+++ main.js
@@ -0,0 +1,87 @@
+const imageInput = document.getElementById("imageInput");
+
+imageInput.addEventListener("change", function (event) {
+  const selectedFile = event.target.files[0];
+
+  if (selectedFile) {
+    document.getElementById(
+      "fileNameText"
+    ).textContent = `Arquivo selecionado: ${selectedFile.name}`;
+  } else {
+    document.getElementById("fileNameText").textContent = "";
+  }
+});
+
+function updateImageCountText() {
+  const numCols = parseInt(document.getElementById("numCols").value);
+  const numRows = parseInt(document.getElementById("numRows").value);
+  const imageCount = numCols * numRows;
+  const imageCountText = `Será gerado ${imageCount} imagens.`;
+  document.getElementById("imageCountText").textContent = imageCountText;
+}
+
+document
+  .getElementById("numCols")
+  .addEventListener("input", updateImageCountText);
+document
+  .getElementById("numRows")
+  .addEventListener("input", updateImageCountText);
+
+function generatePDF() {
+  const imageInput = document.getElementById("imageInput");
+  const numCols = parseInt(document.getElementById("numCols").value);
+  const numRows = parseInt(document.getElementById("numRows").value);
+  const quantity = numCols * numRows;
+  const file = imageInput.files[0];
+
+  if (!file) {
+    alert("Por favor, selecione uma imagem.");
+    return;
+  }
+
+  const reader = new FileReader();
+  reader.onloadend = function () {
+    const imageBase64 = reader.result;
+    const img = new Image();
+    img.src = imageBase64;
+    img.onload = function () {
+      const imgWidth = img.width;
+      const imgHeight = img.height;
+      const pdf = new jsPDF();
+      const margin = 10;
+      const pageWidth = pdf.internal.pageSize.getWidth();
+      const pageHeight = pdf.internal.pageSize.getHeight();
+      const cellWidth = (pageWidth - 2 * margin) / numCols;
+      const cellHeight = (pageHeight - 2 * margin) / numRows;
+
+      let currentRow = 0;
+      let currentCol = 0;
+
+      for (let i = 0; i < quantity; i++) {
+        const x = margin + currentCol * cellWidth;
+        const y = margin + currentRow * cellHeight;
+
+        const scaleFactor = Math.min(
+          cellWidth / imgWidth,
+          cellHeight / imgHeight
+        );
+        const newWidth = imgWidth * scaleFactor;
+        const newHeight = imgHeight * scaleFactor;
+
+        pdf.addImage(imageBase64, "JPEG", x, y, newWidth, newHeight);
+        currentCol++;
+        if (currentCol === numCols) {
+          currentCol = 0;
+          currentRow++;
+          if (currentRow === numRows) {
+            currentRow = 0;
+          }
+        }
+      }
+
+      pdf.save("output.pdf");
+    };
+  };
+
+  reader.readAsDataURL(file);
+}
